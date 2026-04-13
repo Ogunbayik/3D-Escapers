ac9ff600b1888bd01af1cfa2fc3047329cebbe2d
Ogunbayik <ogun.bayikk@gmail.com>
2026-04-13 19:56:39 +0300

    feat(player): implement jump mechanics and integrate with movement system

--- Assets/_Project/Scripts/PlayerBase.cs
+++ Assets/_Project/Scripts/PlayerBase.cs
@@ -6,17 +6,44 @@ public class PlayerBase : MonoBehaviour
 {
     private CharacterController _characterController;
 
+    [Header("Visual")]
+    [SerializeField] private Transform _playerVisual;
+    [Header("Movement")]
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _rotationSpeed;
+    [Header("Jump")]
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _gravityMultiplier;
+    [Header("Checking")]
+    [SerializeField] private Transform _checkTransform;
+    [SerializeField] private float _checkDistance;
+    [SerializeField] private LayerMask _checkLayer;
 
-    [SerializeField] private Transform _playerVisual;
 
     private Vector3 _movementDirection;
+
+    private Vector3 _velocity;
+
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
     void Update()
+    {
+        if (PressedJump() && IsGround())
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+
+            HandleMovement();
+        HandleRotation();
+
+        _velocity.y += _gravity * _gravityMultiplier * Time.deltaTime;
+        _characterController.Move(_velocity * Time.deltaTime);
+
+        var isGround = Physics.CheckSphere(_checkTransform.position, _checkDistance, _checkLayer);
+    }
+
+    private void HandleMovement()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
@@ -28,8 +55,6 @@ public class PlayerBase : MonoBehaviour
 
         if (IsMoving())
             _characterController.Move(_movementDirection * _movementSpeed * Time.deltaTime);
-
-        HandleRotation();
     }
 
     private void HandleRotation()
@@ -40,6 +65,14 @@ public class PlayerBase : MonoBehaviour
             _playerVisual.transform.rotation = Quaternion.Slerp(_playerVisual.transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
         }
     }
-
+    private bool IsGround() => Physics.CheckSphere(_checkTransform.position, _checkDistance, _checkLayer);
+    private bool PressedJump() => Input.GetKeyDown(KeyCode.Space);
     private bool IsMoving() => _movementDirection != Vector3.zero;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(_checkTransform.transform.position, _checkDistance);
+    }
 }
